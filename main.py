2ca08eeba63bee3c83215fbbbc4db79861fb7e25
ESHUshri202 <eshushrivastava1106@gmail.com>
2024-08-02 02:10:44 +0530

    commit

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def get_predicted_value(patient_symptoms):
         input_vector[symptoms_dict[item]] = 1
     return diseases_list[svc.predict([input_vector])[0]]
 #creating routes 
-@app.route('/templates/index.html')
+@app.route('/')
 def index():
     return render_template('index.html')
 
